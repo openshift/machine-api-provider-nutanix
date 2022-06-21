2701e2685b207ca8e3a29367e118274357918f10
Yanhua Li <yanhua.li@nutanix.com>
2022-06-21 15:40:42 -0400

    Fixing Bug 2094335: [Nutanix] - debug log should not be enabled by default

--- pkg/actuators/machine/machine_scope.go
+++ pkg/actuators/machine/machine_scope.go
@@ -91,9 +91,10 @@ func newMachineScope(params machineScopeParams) (*machineScope, error) {
 
 func (s *machineScope) getNutanixClientOptions() (*clientpkg.ClientOptions, error) {
 
+	debugEnabled := klog.V(5).Enabled()
 	clientOptions := &clientpkg.ClientOptions{
 		Credentials: &nutanixClient.Credentials{},
-		Debug:       true,
+		Debug:       debugEnabled,
 	}
 
 	// Get the PC endpoint/port from the Infrastructure CR
