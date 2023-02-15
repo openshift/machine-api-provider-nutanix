6c664207e5aef337efb45a2f03093a8d13d5a3b1
Daniel Odvarka <odvarkaadaniel@gmail.com>
2023-02-15 14:05:47 +0100

    Update k8s packages to 1.26

--- pkg/actuators/machineset/suite_test.go
+++ pkg/actuators/machineset/suite_test.go
@@ -29,7 +29,6 @@ import (
 	"k8s.io/klog/v2"
 	"k8s.io/klog/v2/klogr"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
@@ -55,9 +54,8 @@ var (
 func TestMachinesetController(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"MachineSet Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t,
+		"MachineSet Controller Suite")
 }
 
 var _ = BeforeSuite(func(done Done) {
