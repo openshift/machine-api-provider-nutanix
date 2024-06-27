9d774cec10800445b12eea061d615b6ac06b8090
Nolan Brubaker <nolan@nbrubaker.com>
2024-06-27 11:24:23 -0400

    Add latest CRD package to imports
    Signed-off-by: Nolan Brubaker <nolan@nbrubaker.com>

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,7 @@ package tools
 
 import (
 	_ "github.com/golang/mock/mockgen"
+	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
