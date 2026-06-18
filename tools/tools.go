0752177afb8e202814145d6d9032c0593e403abe
Brandon Palm <bpalm@redhat.com>
2026-06-18 12:36:25 -0500

    mockgen deprecated: use uber-go/mock instead

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@
 package tools
 
 import (
-	_ "github.com/golang/mock/mockgen"
+	_ "go.uber.org/mock/mockgen"
 	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
 	_ "github.com/openshift/api/machine/v1beta1/zz_generated.crd-manifests"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
